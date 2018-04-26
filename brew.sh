ce28849be2c6a4fbfe865421cb63388819d646d4
Johan Meiring <johanmeiring@gmail.com>
2018-04-26 15:49:37 +0200

    Install xdebug properly

--- brew.sh
+++ brew.sh
@@ -100,10 +100,12 @@ brew install amazon-ecs-cli
 brew install aws-elasticbeanstalk
 brew install ec2-api-tools
 
-# Install PHP (specifically for minimal CakePHP development).
+# Install PHP and xdebug (specifically for minimal CakePHP development).
 brew install php@7.1
 brew link php@7.1 --force --overwrite
 brew install composer
+pecl install xdebug
+cp /usr/local/lib/php/pecl/20160303/xdebug.so /usr/local/lib/php/20160303/.
 
 # Install Go and related tools.
 brew install go
