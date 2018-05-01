c7a87a0c07804def3b9532faa520bfaaa895dbc6
Johan Meiring <johanmeiring@gmail.com>
2018-05-01 20:28:50 +0200

    Replace fisherman with oh-my-fish

--- fish.sh
+++ fish.sh
@@ -6,11 +6,10 @@ command -v fish > /dev/null || \
         echo $(brew --prefix)/bin/fish | sudo tee -a /etc/shells && \
         chsh -s $(brew --prefix)/bin/fish)
 
-# Install fisherman
-[[ ! -f ~/.config/fish/functions/fisher.fish ]] && curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisherman
+# Install oh-my-fish (https://github.com/oh-my-fish/oh-my-fish)
+[[ ! -f ~/.config/fish/conf.d/omf.fish ]] && curl -L https://get.oh-my.fish | fish
 
 # Install Powerline fonts
 git clone https://github.com/powerline/fonts.git /tmp/powerline && /tmp/powerline/install.sh
 
-# Install bobthefish theme
-#fisher omf/theme-bobthefish
+exit 0
