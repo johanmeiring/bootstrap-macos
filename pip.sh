aebd5a528f8a0ae177f1a9e56f46b967bfba03e2
Johan Meiring <johan@meiring.net>
2021-12-06 11:39:39 +0200

    Install latest boto as per Ansible EC2 inventory plugin requirements

--- pip.sh
+++ pip.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # pip3 install virtualenv
-pip3 install boto
+pip3 install boto3
+pip3 install botocore
 pip3 install docker
 pip3 install powerline-status
 pip3 install --user ansible
